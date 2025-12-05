7a2be4b51c49f144960057acb55cee231f10b86f
markoopapa <markoopapa@ymail.com>
2025-12-05 08:55:16 +0200

    Create en.php

---
+++ translations/en.php
@@ -0,0 +1,20 @@
+<?php
+global $_MODULE;
+$_MODULE = array();
+
+// Main module
+$_MODULE['<{mpstocksync}prestashop>mpstocksync_2dd41365c5c4ecb5f0f54e8a919d4c1c'] = 'MP Stock Sync Pro';
+$_MODULE['<{mpstocksync}prestashop>mpstocksync_c9cc8cce247e49bae79f15173ce97354'] = 'Save';
+$_MODULE['<{mpstocksync}prestashop>mpstocksync_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'eMAG and Trendyol stock synchronization';
+
+// Dashboard
+$_MODULE['<{mpstocksync}prestashop>dashboard_5dd985f6a5f6b5c5c5c5c5c5c5c5c5c5'] = 'Dashboard';
+$_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'eMAG Status';
+$_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c6'] = 'Trendyol Status';
+$_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c7'] = 'Product Mapping';
+$_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c8'] = 'Sync Logs';
+
+// Buttons
+$_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'Sync Now';
+$_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c6'] = 'Test Connection';
+$_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c7'] = 'Save Settings';
