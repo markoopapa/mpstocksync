dec17d5d30e53e216c7a1f7e35218f23c96babdb
markoopapa <markoopapa@ymail.com>
2025-12-05 07:26:47 +0200

    Create autoload.php

---
+++ vendor/autoload.php
@@ -0,0 +1,18 @@
+<?php
+// Autoloader for PSR-4
+spl_autoload_register(function ($class) {
+    $prefix = 'MpStockSync\\';
+    $base_dir = __DIR__ . '/../src/';
+    
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
