adfa8f37fab5b5f6dca7463adb405d984d9ffb0b
markoopapa <markoopapa@ymail.com>
2025-12-05 10:09:52 +0200

    Update hu.php

--- translations/hu.php
+++ translations/hu.php
@@ -2,6 +2,7 @@
 global $_MODULE;
 $_MODULE = array();
 
+// Fő modul
 $_MODULE['<{mpstocksync}prestashop>mpstocksync_2dd41365c5c4ecb5f0f54e8a919d4c1c'] = 'MP Stock Sync Pro';
 $_MODULE['<{mpstocksync}prestashop>mpstocksync_c9cc8cce247e49bae79f15173ce97354'] = 'Mentés';
 $_MODULE['<{mpstocksync}prestashop>mpstocksync_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'eMAG és Trendyol készletszinkronizálás';
@@ -13,19 +14,25 @@ $_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c6'] 
 $_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c7'] = 'Termék összekapcsolás';
 $_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c8'] = 'Szinkronizálási naplók';
 
-// API Settings
-$_MODULE['<{mpstocksync}prestashop>api_settings_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c9'] = 'API beállítások';
-$_MODULE['<{mpstocksync}prestashop>api_settings_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d'] = 'eMAG API konfiguráció';
-$_MODULE['<{mpstocksync}prestashop>api_settings_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5e'] = 'Trendyol API konfiguráció';
-
-// Product Mapping
-$_MODULE['<{mpstocksync}prestashop>product_mapping_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5f'] = 'Termék összekapcsolás';
-$_MODULE['<{mpstocksync}prestashop>product_mapping_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c60'] = 'Platform';
-$_MODULE['<{mpstocksync}prestashop>product_mapping_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c61'] = 'Külső azonosító';
-$_MODULE['<{mpstocksync}prestashop>product_mapping_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c62'] = 'Terméknév';
-$_MODULE['<{mpstocksync}prestashop>product_mapping_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c63'] = 'Utolsó szinkronizálás';
-
-// Buttons
+// Gombok
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'Szinkronizálás most';
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c6'] = 'Kapcsolat tesztelése';
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c7'] = 'Beállítások mentése';
+
+// API beállítások
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c8'] = 'API URL';
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c9'] = 'API kulcs';
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d0'] = 'API titok';
+
+// Beszállítók
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d1'] = 'Beszállítók';
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d2'] = 'Adatbázis kapcsolat';
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d3'] = 'Automatikus szinkronizálás';
+
+// Általános
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d4'] = 'Aktív';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d5'] = 'Inaktív';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d6'] = 'Sikeres';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d7'] = 'Sikertelen';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d8'] = 'Megerősítés';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d9'] = 'Művelet';
