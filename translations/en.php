6cccd79f0b503b38ea3719b5e81db81e16f132e4
markoopapa <markoopapa@ymail.com>
2025-12-05 10:10:09 +0200

    Update en.php

--- translations/en.php
+++ translations/en.php
@@ -18,3 +18,21 @@ $_MODULE['<{mpstocksync}prestashop>dashboard_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c8'] 
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c5'] = 'Sync Now';
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c6'] = 'Test Connection';
 $_MODULE['<{mpstocksync}prestashop>buttons_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c7'] = 'Save Settings';
+
+// API Settings
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c8'] = 'API URL';
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5c9'] = 'API Key';
+$_MODULE['<{mpstocksync}prestashop>api_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d0'] = 'API Secret';
+
+// Suppliers
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d1'] = 'Suppliers';
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d2'] = 'Database Connection';
+$_MODULE['<{mpstocksync}prestashop>suppliers_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d3'] = 'Auto Sync';
+
+// General
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d4'] = 'Active';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d5'] = 'Inactive';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d6'] = 'Success';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d7'] = 'Failed';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d8'] = 'Confirm';
+$_MODULE['<{mpstocksync}prestashop>general_5e5b5cc6e3d9e7f5c5c5c5c5c5c5c5d9'] = 'Action';
