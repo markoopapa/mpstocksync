2283e31b5b47643bbdc29245f809035fef15b66c
markoopapa <markoopapa@ymail.com>
2025-12-06 10:27:43 +0200

    Update AdminMpStockSyncSuppliersController.php

--- controllers/admin/AdminMpStockSyncSuppliersController.php
+++ controllers/admin/AdminMpStockSyncSuppliersController.php
@@ -1,70 +1,64 @@
 <?php
-require_once _PS_MODULE_DIR_ . 'mpstocksync/classes/MpStockSyncMapping.php';
-
+// Basic admin controller to list suppliers and trigger test/sync
 class AdminMpStockSyncSuppliersController extends ModuleAdminController
 {
     public function __construct()
     {
+        $this->table = 'mpstocksync_suppliers';
+        $this->className = 'MpStockSyncSupplier';
         $this->bootstrap = true;
-        $this->table = 'supplier';
-        $this->identifier = 'id_supplier';
-        $this->className = 'Supplier';
-        $this->lang = false;
-        $this->list_no_link = true;
-
         parent::__construct();
-
-        $this->fields_list = array(
-            'id_supplier' => array(
-                'title' => $this->l('ID'),
-                'align' => 'center',
-                'width' => 30
-            ),
-            'name' => array(
-                'title' => $this->l('Name'),
-                'width' => 'auto'
-            ),
-            'mpstocksync_active' => array(
-                'title' => $this->l('Active'),
-                'active' => 'status',
-                'type' => 'bool',
-                'align' => 'center',
-                'orderby' => false
-            )
-        );
-
-        $this->bulk_actions = array(
-            'enable' => array(
-                'text' => $this->l('Enable'),
-                'icon' => 'icon-power-off text-success'
-            ),
-            'disable' => array(
-                'text' => $this->l('Disable'),
-                'icon' => 'icon-power-off text-danger'
-            )
-        );
     }
 
-    public function initPageHeaderToolbar()
+    public function initContent()
     {
-        parent::initPageHeaderToolbar();
-        $this->page_header_toolbar_btn['sync_suppliers'] = array(
-            'href' => self::$currentIndex . '&syncSuppliers&token=' . $this->token,
-            'desc' => $this->l('Sync Suppliers'),
-            'icon' => 'icon-refresh'
-        );
+        parent::initContent();
+
+        $suppliers = Db::getInstance()->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'mpstocksync_suppliers');
+
+        $this->context->smarty->assign([
+            'suppliers' => $suppliers,
+            'token' => $this->token
+        ]);
+
+        $this->setTemplate('supplier_config.tpl');
     }
 
+    // handle ajax test_connection and sync actions
     public function postProcess()
     {
-        if (Tools::getValue('syncSuppliers')) {
-            $this->syncSuppliers();
+        if (Tools::isSubmit('test_connection') && Tools::getValue('id_supplier')) {
+            $id = (int)Tools::getValue('id_supplier');
+            $supplier = Db::getInstance()->getRow('SELECT * FROM ' . _DB_PREFIX_ . 'mpstocksync_suppliers WHERE id_supplier = ' . $id);
+            if ($supplier) {
+                $client = new \MpStockSync\ApiClient\SupplierApiClient($supplier['api_url'], $supplier['api_key']);
+                $res = $client->getProducts();
+                if ($res['success']) {
+                    $this->confirmations[] = 'Connection OK, products fetched: ' . count($res['products']);
+                } else {
+                    $this->errors[] = 'Connection failed: ' . ($res['error'] ?? 'unknown');
+                }
+            }
         }
-        parent::postProcess();
-    }
 
-    private function syncSuppliers()
-    {
-        $this->confirmations[] = $this->l('Suppliers synchronized successfully');
+        if (Tools::isSubmit('sync_supplier') && Tools::getValue('id_supplier')) {
+            $id = (int)Tools::getValue('id_supplier');
+            $supplier = Db::getInstance()->getRow('SELECT * FROM ' . _DB_PREFIX_ . 'mpstocksync_suppliers WHERE id_supplier = ' . $id);
+            if ($supplier) {
+                $config = [
+                    'api_url' => $supplier['api_url'],
+                    'api_key' => $supplier['api_key'],
+                    'id_supplier' => $supplier['id_supplier'],
+                    'target_shops' => $supplier['target_shops']
+                ];
+                $svc = new \MpStockSync\Service\SupplierSyncService($config);
+                $summary = $svc->sync();
+                if ($summary['success']) {
+                    $this->confirmations[] = 'Sync completed: updated ' . $summary['updated'] . ' products';
+                } else {
+                    $this->errors[] = 'Sync failed: ' . implode('; ', $summary['errors']);
+                }
+            }
+        }
     }
 }
